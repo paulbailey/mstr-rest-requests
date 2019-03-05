2ad33550802a6ffe71ea323fc48f4889127d1d63
Paul Bailey <paul.bailey@rinbo.consulting>
2019-03-05 12:42:44 +0000

    Fix package description

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.0.1",
     author="Paul Bailey",
     author_email="bailey@dreamshake.net",
-    description="A custom subclass of sessions Session object for interacting with the MicroStrategy REST API",
+    description="Easily make requests to the MicroStrategy REST API",
     packages=setuptools.find_packages(),
     install_requires=[
         "requests",
