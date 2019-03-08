d45c113ee3f086fca5a6bd92a02c1fe90335e444
Paul Bailey <paul.bailey@rinbo.consulting>
2019-03-08 11:56:51 +0000

    Build into the right directory

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mstr-rest-requests",
-    version="0.0.7",
+    version="0.0.8",
     author="Paul Bailey",
     author_email="bailey@dreamshake.net",
     description="Easily make requests to the MicroStrategy REST API",
