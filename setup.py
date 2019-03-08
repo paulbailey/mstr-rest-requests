d69471ce7b559a4fd5c9c5a2a78bacc0ebcc51ab
Paul Bailey <paul.bailey@rinbo.consulting>
2019-03-08 11:51:02 +0000

    Serverless

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ import setuptools
 
 setuptools.setup(
     name="mstr-rest-requests",
-    version="0.0.6",
+    version="0.0.7",
     author="Paul Bailey",
     author_email="bailey@dreamshake.net",
     description="Easily make requests to the MicroStrategy REST API",
