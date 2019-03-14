9add3e1d7159bd489cbf996e4c0fb61208d31a14
Paul Bailey <bailey@dreamshake.net>
2019-03-14 13:58:28 +0000

    Tweak setuptools stuff

--- setup.py
+++ setup.py
@@ -1,14 +1,6 @@
 import setuptools
 
 setuptools.setup(
-    name="mstr-rest-requests",
-    version="0.0.8",
-    author="Paul Bailey",
-    author_email="bailey@dreamshake.net",
-    description="Easily make requests to the MicroStrategy REST API",
-    packages=setuptools.find_packages(),
-    install_requires=[
-        "requests",
-        "requests-toolbelt"
-    ]
+    setup_requires="pytest-runner",
+    tests_require="pytest",
 )
