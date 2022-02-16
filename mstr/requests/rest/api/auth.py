5f4bda0b6df32d5940804831c44ad3d699828220
Paul Bailey <bailey@dreamshake.net>
2022-02-16 15:46:24 +0000

    Allow session delegation

--- mstr/requests/rest/api/auth.py
+++ mstr/requests/rest/api/auth.py
@@ -59,3 +59,11 @@ class AuthMixin:
     def logout(self):
         """Closes the REST API session associated with the session object."""
         return self.post_logout()
+
+    def delegate(self, identity_token: str):
+        delegate_response = self.post(
+            "auth/delegate", json={"loginMode": -1, "identityToken": identity_token}
+        )
+        if delegate_response.status_code != 204:
+            delegate_response.raise_for_status()
+        return delegate_response
