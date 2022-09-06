a8d9070cecd3b368191baf2f590de37b8cb71203
Paul Bailey <bailey@dreamshake.net>
2022-09-06 13:55:13 +0100

    Remove redundant tests

--- tests/test_projects.py
+++
@@ -1,9 +0,0 @@
-from mstr.requests import AuthenticatedMSTRRESTSession
-
-
-def test_list_projects():
-    with AuthenticatedMSTRRESTSession(
-        base_url="https://demo.microstrategy.com/MicroStrategyLibrary/api/"
-    ) as session:
-        projects = session.get_projects()
-        assert projects
